331ee94adfdab5b741697043c9706e2ba2ea9ba9
Amélie Grenier <amelie.grenier@hotmail.fr>
2023-06-14 17:13:05 +0200

    add submit logique + design on input lastname

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { SyntheticEvent, useState } from 'react'
 import { Flex, Box, Button, Container, FormControl, FormHelperText, Input, Heading, Text, Highlight, Stack, InputGroup, InputRightElement, Image } from '@chakra-ui/react'
-import firstNameErrorIcon from './images/icon-error.svg'
+import ErrorIcon from './images/icon-error.svg'
 
 type input = {
   firstName: string,
@@ -17,20 +17,19 @@ export function App () {
     password: ''
   })
   const [firstNameError, setFirstNameError] = useState<boolean>(false)
+  const [lastNameError, setLastNameError] = useState<boolean>(false)
+  // const [emailError, setEmailError] = useState<boolean>(false)
+  // const [passwordError, setPasswordError] = useState<boolean>(false)
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
     setFirstNameError(false)
-    if (input.firstName === '') {
+    setLastNameError(false)
+    if (input.firstName === '' || input.lastName === '' || input.email || input.password) {
       setFirstNameError(true)
-    }
-
-    if (input.lastName === '') {
-      setFirstNameError(true)
+      setLastNameError(true)
     }
   }
-
-  console.log(firstNameError)
 
   return (
     <Container maxW='container.m' h='100vh' bgImage="url('./images/bg-intro-desktop.png')" bgColor='hsl(0, 100%, 74%)'>
@@ -47,16 +46,22 @@ export function App () {
           <FormControl as='form' onSubmit={handleSubmit} display={'flex'} flexDirection="column" alignContent="center" bgColor={'white'} p={10} borderRadius={10} boxShadow='lg' >
             <Stack spacing={3}>
               <InputGroup display={'flex'} flexDirection={'column'}>
-                <InputRightElement >
-                  {input.firstName === '' && firstNameError ? <Image height={4} src={firstNameErrorIcon}/> : ''}
+                <InputRightElement>
+                  {input.firstName === '' && firstNameError ? <Image height={4} src={ErrorIcon}/> : ''}
                 </InputRightElement>
-                <Input id='firsName' borderColor={input.firstName === '' && firstNameError ? 'hsl(0, 100%, 74%)' : ''} fontWeight={'500'} fontSize={14} placeholder='First Name' value={input.firstName || ''} onChange={(e) => setInput({ ...input, firstName: e.target.value })}/>
+                <Input id='firsName' borderColor={input.firstName === '' && firstNameError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='First Name' value={input.firstName || ''} onChange={(e) => setInput({ ...input, firstName: e.target.value })}/>
                 {(firstNameError && input.firstName === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'} >First Name cannot be empty</Text>}
               </InputGroup>
-              <Input id='lasName' fontWeight={'500'} fontSize={14} placeholder='Last Name' value={input.lastName || ''} onChange={(e) => setInput({ ...input, lastName: e.target.value })}/>
+              <InputGroup display={'flex'} flexDirection={'column'}>
+                <InputRightElement >
+                  {input.lastName === '' && lastNameError ? <Image height={4} src={ErrorIcon}/> : ''}
+                </InputRightElement>
+                <Input id='lasName' borderColor={input.lastName === '' && lastNameError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='Last Name' value={input.lastName || ''} onChange={(e) => setInput({ ...input, lastName: e.target.value })}/>
+                {(lastNameError && input.lastName === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'}>Last Name cannot be empty</Text>}
+              </InputGroup>
               <Input id='email' type='email' fontWeight={'500'} fontSize={14} placeholder='Email Address' value={input.email || ''} onChange={(e) => setInput({ ...input, email: e.target.value })}/>
               <Input id='password' type='password' fontWeight={'500'} fontSize={14} placeholder='Password' value={input.password || ''} onChange={(e) => setInput({ ...input, password: e.target.value })}/>
-              <Button onClick={handleSubmit} cursor={'pointer'} w={'100%'} color={'white'} bgColor={'hsl(154, 59%, 51%)'} boxShadow='Inner'>Claim your free trial </Button>
+              <Button onClick={handleSubmit} cursor={'pointer'} w={'100%'} color={'white'} bgColor={'hsl(154, 59%, 51%)'}>Claim your free trial </Button>
             </Stack>
             <FormHelperText fontSize={'8px'} pl={'24px'}>
               <Highlight query='Terms and Services' styles={{ color: 'hsl(0, 100%, 74%)', fontWeight: '700' }}>By clicking the button, you are agreeing to our Terms and Services</Highlight></FormHelperText>
