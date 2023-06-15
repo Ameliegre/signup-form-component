ab703ce415cb1e5ae820173ee41395b4f9c575a3
Amélie Grenier <amelie.grenier@hotmail.fr>
2023-06-15 14:33:31 +0200

    add error message email input

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ type input = {
   password: string
 }
 
+const emailREGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
+
 export function App () {
   const [input, setInput] = useState<input>({
     firstName: '',
@@ -19,6 +21,7 @@ export function App () {
   const [firstNameError, setFirstNameError] = useState<boolean>(false)
   const [lastNameError, setLastNameError] = useState<boolean>(false)
   const [emailError, setEmailError] = useState<boolean>(false)
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(false)
   const [passwordError, setPasswordError] = useState<boolean>(false)
 
   const handleSubmit = (e: SyntheticEvent) => {
@@ -27,13 +30,22 @@ export function App () {
     setLastNameError(false)
     setEmailError(false)
     setPasswordError(false)
-    if (input.firstName === '' || input.lastName === '' || input.email || input.password) {
+    if (input.firstName === '' || input.lastName === '' || input.email === '' || input.password === '') {
       setFirstNameError(true)
       setLastNameError(true)
       setEmailError(true)
       setPasswordError(true)
     }
+
+    const emailValid = emailREGEX.test(input.email)
+    setIsEmailValid(false)
+
+    if (!emailValid) {
+      setIsEmailValid(true)
+    }
   }
+
+  console.log(isEmailValid, emailError)
 
   return (
     <Container maxW='container.m' h='100vh' bgImage="url('./images/bg-intro-desktop.png')" bgColor='hsl(0, 100%, 74%)'>
@@ -49,34 +61,39 @@ export function App () {
           </Text>
           <FormControl as='form' onSubmit={handleSubmit} display={'flex'} flexDirection="column" alignContent="center" bgColor={'white'} p={10} borderRadius={10} boxShadow='0px 5px rgba(0, 0, 0, 0.15)' >
             <Stack spacing={3}>
+
               <InputGroup display={'flex'} flexDirection={'column'}>
                 <InputRightElement>
-                  {input.firstName === '' && firstNameError ? <Image height={4} src={ErrorIcon}/> : ''}
+                  {input.firstName === '' && firstNameError ? <Image height={4} src={ErrorIcon}/> : <></>}
                 </InputRightElement>
                 <Input id='firsName' borderColor={input.firstName === '' && firstNameError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='First Name' value={input.firstName || ''} onChange={(e) => setInput({ ...input, firstName: e.target.value })}/>
                 {(firstNameError && input.firstName === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'} >First Name cannot be empty</Text>}
               </InputGroup>
+
               <InputGroup display={'flex'} flexDirection={'column'}>
                 <InputRightElement >
-                  {input.lastName === '' && lastNameError ? <Image height={4} src={ErrorIcon}/> : ''}
+                  {input.lastName === '' && lastNameError ? <Image height={4} src={ErrorIcon}/> : <></>}
                 </InputRightElement>
                 <Input id='lasName' borderColor={input.lastName === '' && lastNameError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='Last Name' value={input.lastName || ''} onChange={(e) => setInput({ ...input, lastName: e.target.value })}/>
                 {(lastNameError && input.lastName === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'}>Last Name cannot be empty</Text>}
               </InputGroup>
+
               <InputGroup display={'flex'} flexDirection={'column'}>
                 <InputRightElement >
-                    {input.email === '' && emailError ? <Image height={4} src={ErrorIcon}/> : ''}
+                    {input.email === '' && emailError ? <Image height={4} src={ErrorIcon}/> : <></>}
                 </InputRightElement>
                 <Input id='email' borderColor={input.email === '' && emailError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='Email Address' value={input.email || ''} onChange={(e) => setInput({ ...input, email: e.target.value })}/>
-                {(lastNameError && input.lastName === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'}>Email cannot be empty</Text>}
+                {(emailError || isEmailValid || input.email === '') && (<Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'}>Looks like this is not an email</Text>)}
               </InputGroup>
+
               <InputGroup display={'flex'} flexDirection={'column'}>
                 <InputRightElement>
-                      {input.password === '' && passwordError ? <Image height={4} src={ErrorIcon}/> : ''}
+                      {input.password === '' && passwordError ? <Image height={4} src={ErrorIcon}/> : <></>}
                   </InputRightElement>
                   <Input id='password' type='password' borderColor={input.password === '' && passwordError ? 'hsl(0, 100%, 74%)' : 'gray.200'} fontWeight={'500'} fontSize={14} placeholder='Password' value={input.password || ''} onChange={(e) => setInput({ ...input, password: e.target.value })}/>
                   {(passwordError && input.password === '') && <Text as='i' pt={'4px'} alignSelf={'flex-end'} fontSize={10} color={'hsl(0, 100%, 74%)'}>Password cannot be empty</Text>}
-                </InputGroup>
+              </InputGroup>
+
               <Button onClick={handleSubmit} cursor={'pointer'} w={'100%'} color={'white'} bgColor={'hsl(154, 59%, 51%)'}>CLAIM YOUR FREE TRIAL</Button>
             </Stack>
             <FormHelperText fontSize={'8px'} pl={'24px'}>
